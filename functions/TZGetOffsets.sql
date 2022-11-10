16f842bc74746d514cc7bbb9ca1601c9c56b1357
jobbish-sql <33984311+jobbish-sql@users.noreply.github.com>
2022-11-10 14:04:37 -0600

    Update TZGetOffsets.sql

--- functions/TZGetOffsets.sql
+++ functions/TZGetOffsets.sql
@@ -1,6 +1,9 @@
 /*
 Name: TZGetOffsetsDT
-Purpose: returns a pair of offset values to be used as parameters for dbo.TZFormatDT
+Purpose: Returns a pair of offset values to be used as parameters for dbo.TZFormatDT
+License: MIT
+Author: Joe Obbish
+Full Source Code: https://github.com/idsdavidgutierrez/fit-sql-faster-time-zone-conversions
 Parameters:
 	@Input - the datetime value to be converted
 	@SourceTimeZoneName - the source time zone of the input in Windows standard format (see sys.time_zone_info)
@@ -19,6 +22,8 @@ RETURN (
 	FROM dbo.TimeZoneConversionHelper_CCI l
 	WHERE l.SourceTimeZoneNameChecksum = CHECKSUM(UPPER(@SourceTimeZoneName) COLLATE Latin1_General_100_BIN2)
 	AND l.TargetTimeZoneNameChecksum = CHECKSUM(UPPER(@TargetTimeZoneName) COLLATE Latin1_General_100_BIN2)
+	--AND l.SourceTimeZoneName = @SourceTimeZoneName
+	--AND l.TargetTimeZoneName = @TargetTimeZoneName
 	AND l.YearBucket = DATEPART(YEAR, @Input)
 	AND l.IntervalStart <= CAST(@Input AS DATETIME2(7))
 	AND l.IntervalEnd > CAST(@Input AS DATETIME2(7))
@@ -28,7 +33,10 @@ GO
 
 /*
 Name: TZGetOffsetsDT2
-Purpose: returns a pair of offset values to be used as parameters for dbo.TZFormatDT2
+Purpose: Returns a pair of offset values to be used as parameters for dbo.TZFormatDT2
+License: MIT
+Author: Joe Obbish
+Full Source Code: https://github.com/idsdavidgutierrez/fit-sql-faster-time-zone-conversions
 Parameters:
 	@Input - the datetime2 value to be converted
 	@SourceTimeZoneName - the source time zone of the input in Windows standard format (see sys.time_zone_info)
@@ -47,6 +55,8 @@ RETURN (
 	FROM dbo.TimeZoneConversionHelper_CCI l
 	WHERE l.SourceTimeZoneNameChecksum = CHECKSUM(UPPER(@SourceTimeZoneName) COLLATE Latin1_General_100_BIN2)
 	AND l.TargetTimeZoneNameChecksum = CHECKSUM(UPPER(@TargetTimeZoneName) COLLATE Latin1_General_100_BIN2)
+	--AND l.SourceTimeZoneName = @SourceTimeZoneName
+	--AND l.TargetTimeZoneName = @TargetTimeZoneName
 	AND l.YearBucket = DATEPART(YEAR, @Input)
 	AND l.IntervalStart <= @Input
 	AND l.IntervalEnd > @Input
@@ -57,7 +67,10 @@ GO
 
 /*
 Name: TZGetOffsetsDTO
-Purpose: returns a pair of offset values to be used as parameters for dbo.TZFormatDTO
+Purpose: Returns a pair of offset values to be used as parameters for dbo.TZFormatDTO
+License: MIT
+Author: Joe Obbish
+Full Source Code: https://github.com/idsdavidgutierrez/fit-sql-faster-time-zone-conversions
 Parameters:
 	@Input - the datetime2 value to be converted
 	@TargetTimeZoneName - the target time zone of the input in Windows standard format (see sys.time_zone_info)
@@ -74,6 +87,8 @@ RETURN (
 	FROM dbo.TimeZoneConversionHelper_CCI l
 	WHERE l.SourceTimeZoneNameChecksum = CHECKSUM(UPPER(N'UTC') COLLATE Latin1_General_100_BIN2)
 	AND l.TargetTimeZoneNameChecksum = CHECKSUM(UPPER(@TargetTimeZoneName) COLLATE Latin1_General_100_BIN2)
+	-- AND l.SourceTimeZoneName = N'UTC'
+	-- AND l.TargetTimeZoneName = @TargetTimeZoneName
 	AND l.YearBucket = DATEPART(YEAR, SWITCHOFFSET(@Input, 0))
 	AND l.IntervalStart <= CAST(SWITCHOFFSET(@Input, 0) AS DATETIME2(7))
 	AND l.IntervalEnd > CAST(SWITCHOFFSET(@Input, 0) AS DATETIME2(7))
